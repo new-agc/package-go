cdd91f63e1811b2d90e18ec0ac03583e18992ba0
eddie <dmkim@keti.re.kr>
2022-05-10 14:58:05 +0900

    add message model for scoring

---
+++ msg/msg_score.go
@@ -0,0 +1,26 @@
+package agcmsg
+
+type AnswerForm struct {
+	// Equation string
+	Answer string
+}
+
+type ScoreLogForm struct {
+	No     int     `json:"no"`
+	Weight int     `json:"weight"`
+	Status string  `json:"status"`
+	Score  float64 `json:"score"`
+}
+
+type ScoringData struct {
+	Id              string  `json:"id"`
+	Status          string  `json:"status"`
+	Score           float64 `json:"score"`
+	ScoreLog        string  `json:"score_log,omitempty"`
+}
+
+type AGCTestMessage struct {
+	TeamName		string			`json:"team"`
+	AnswerSheet		[]string		`json:"answer_sheet"`
+	Status			string			`json:"status"`
+}
